1a1d03132ced8b1ed28aee7ca4a2819d54fb206f
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-15 22:07:17 +0600

    set up env config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import express, { json, type Application, type Request, type Response } from "express"
 import { Pool } from "pg"
+import config from "./config";
 
 const app: Application = express()
-const port = 3000
+const port = config.port;
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_LAWV3So9yDHx@ep-crimson-frog-aq2txkhb-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  connectionString: config.connection_string
 })
 
 const initDB = async () => {
