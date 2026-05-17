f194fe6d2722b1862d8ce1d4bbda9dfd169e8b37
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-18 00:32:50 +0600

    jwt authentication

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json, type Application, type Request, type Response } from "ex
 import config from "./config";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
+import { authRoute } from "./modules/auth/auth.route";
 
 const app: Application = express()
 // const port = config.port;
@@ -21,6 +22,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use("/api/users", userRoute)
 app.use("/api/profiles", profileRoute)
+app.use("/api/auth" , authRoute)
 
 
 export default app
